a2954ce378001f1aa7a195c9e2bd6a3f7c2bd2b6
Tarun <206566851+Tarun-004@users.noreply.github.com>
2026-06-02 12:49:25 +0530

    Add files via upload

---
+++ Main .py
@@ -0,0 +1,31 @@
+from mcp.server.fastmcp import FastMCP
+import requests
+
+mcp = FastMCP("WeatherServer")
+
+API_KEY = "paste_your_api_key"
+
+
+@mcp.tool()
+def get_weather(city: str) -> str:
+    """
+    Get current weather for a city.
+    """
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return "Could not fetch weather."
+
+    data = response.json()
+
+    temp = data["main"]["temp"]
+    description = data["weather"][0]["description"]
+
+    return f"The weather in {city} is {description} with {temp}°C."
+
+
+if __name__ == "__main__":
+    mcp.run()
